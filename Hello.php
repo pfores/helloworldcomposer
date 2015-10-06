f2ee8d736584cf0cbff538bc6793be22fd037061
pau <you@example.com>
2015-10-06 19:43:56 +0200

    Primera versio

---
+++ Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/**
+ * Created by PhpStorm.
+ * User: pau
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+class Hello
+{
+
+    public function sayHello()
+    {
+        echo "Hola mon!\n";
+    }
+
+}
---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . "/Hello.php";
+/**
+ * Created by PhpStorm.
+ * User: pau
+ * Date: 6/10/15
+ * Time: 19:33
+ */
+
+$greetings = new Hello();
+
+$greetings->sayHello();
